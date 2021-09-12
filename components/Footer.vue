0bd0fa9ed50a6836deaa1471ddec6e2fb233d0dd
Chuan Jin <chuan.jin.813@gmail.com>
2021-09-12 11:06:10 +0200

    Add more data for China

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,9 @@
 <template>
 
-  <section>
+  <b-container class="text-center">
     <hr>
-    <div class="container">
-      <div class="content has-text-centered">
-        The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-      </div>
-    </div>
-  </section>
+    The source code is licensed
+    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+  </b-container>
 
 </template>
--- pages/china.vue
+++ pages/china.vue
@@ -10,7 +10,15 @@
       class="mb-2"
       >
       <b-card-text>
-        cases: {{ cases }}
+        <ul>
+          <li>Population: {{ population }}</li>
+          <li>Total cases: {{ cases }}</li>
+          <li>Today cases: {{ todayCases }}</li>
+          <li>Total deaths: {{ deaths }}</li>
+          <li>Today deaths: {{ todayDeaths }}</li>
+          <li>Total recovered: {{ recovered }}</li>
+          <li>Today recovered: {{ todayRecovered }}</li>
+        </ul>
       </b-card-text>
 
       <b-button href="/" variant="primary">Go back</b-button>
@@ -25,14 +33,27 @@ export default {
 
   data() {
     return {
+      population: 0,
       cases: 0,
+      deaths: 0,
+      recovered: 0,
+      todayCases: 0,
+      todayDeaths: 0,
+      todayRecovered: 0,
       flag: ""
     }
   },
 
   async created() {
     const data = await this.$axios.$get("https://disease.sh/v3/covid-19/countries/china")
+    this.population = data.population
     this.cases = data.cases
+    this.todayCases = data.todayCases
+    this.deaths = data.deaths
+    this.todayDeaths = data.todayDeaths
+    this.recovered = data.recovered
+    this.todayRecovered = data.todayRecovered
+
     this.flag = data.countryInfo.flag
   }
 

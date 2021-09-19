bf888bbdeaf99819da8a383dc1f882c46487ddf7
Chuan Jin <chuan.jin.813@gmail.com>
2021-09-19 14:00:40 +0200

    Extract Bar chart component

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@
           </ul>
         </b-col>
         <b-col>
-          <Vaccination />
+          <BarChart v-if="loaded" :chartdata="chartdata" :options="options" />
         </b-col>
       </b-row>
     </b-container>
@@ -46,8 +46,53 @@
 
 <script>
 export default {
-  layout: 'default'
+  layout: 'default',
+
+  data() {
+    return {
+      loaded: true,
+      chartdata: {
+        labels: [1,2,3,4,5,6,7],
+        datasets: [{
+          label: 'Vaccination',
+          barPercentage: 0.8,
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(21, 203, 207, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    }
+
+  }
 }
+
+
 </script>
 
 
